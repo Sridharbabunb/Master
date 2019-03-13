91150e93ff661207a74df448f1353a94e3ca7e31
snb <sridharbabu.n.b@oracle.com>
2019-03-13 12:51:25 +0530

    adding phone webservice

--- Microservices/src/main/java/com/us/oracle/controller/getphnumber.java
+++ Microservices/src/main/java/com/us/oracle/controller/getphnumber.java
@@ -1,8 +1,26 @@
 package com.us.oracle.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by snb on 3/13/2019.
  */
-public class getphnumber {
 
-}
+    @RestController
+    public class getphnumber
+    {
+        @RequestMapping("/getEmployephno")
+        public List<String> index() {
+            List<String> strings= new ArrayList<>();
+            strings.add("9876543212");
+            strings.add("9863456789");
+            strings.add("98463579091");
+            return strings;
+        }
+    }
+
+
